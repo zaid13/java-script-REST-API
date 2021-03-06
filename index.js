a7ae34b12df029c0bd30d48f7f1ae5afe98adbf9
zaid bin saeed <47746351+zaid13@users.noreply.github.com>
2021-03-06 06:30:50 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,47 +1,64 @@
 
-const express = require('express');
-const app = express();
 
 
-const Joi = require('joi')
-app.get('/',(req,res)=>{
-    res.send("hel  o")
-})
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+// const express = require('express');
+// const app = express();
 
 
-
-// let mysql = require('mysql');
-// let connection = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: '',
-//     database: 'sampleDB',});
-
-// connection.connect(function(err) {
-//   if (!!err) {
-//     return console.error('error: ' + err.message);
-//   }
-//   else{
-
-//   console.log('Connected to the MySQL server.');
-
-//   }
-// });
-
-
-// app.get('/',function(req,res){
-
-//   connection.query('SELECT * FROM sampleDB',function(  qerror,rows,fields  ){
-//       if(error){
-//         console.log('error');
-
-//       }
-//       else{
-//         console.log('success');
-
-//       }
-
-
-//   });
+// const Joi = require('joi')
+// app.get('/',(req,res)=>{
+//     res.send("hel  o")
 // })
-app.listen(1337);
+
+
+
+// // let mysql = require('mysql');
+// // let connection = mysql.createConnection({
+// //     host: 'localhost',
+// //     user: 'root',
+// //     password: '',
+// //     database: 'sampleDB',});
+
+// // connection.connect(function(err) {
+// //   if (!!err) {
+// //     return console.error('error: ' + err.message);
+// //   }
+// //   else{
+
+// //   console.log('Connected to the MySQL server.');
+
+// //   }
+// // });
+
+
+// // app.get('/',function(req,res){
+
+// //   connection.query('SELECT * FROM sampleDB',function(  qerror,rows,fields  ){
+// //       if(error){
+// //         console.log('error');
+
+// //       }
+// //       else{
+// //         console.log('success');
+
+// //       }
+
+
+// //   });
+// // })
+// app.listen(1337);
