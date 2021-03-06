4e81b4849369914f2accafc7a1f126d678d828aa
zaid bin saeed <47746351+zaid13@users.noreply.github.com>
2021-03-06 06:21:04 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,47 @@
-const Joi = require('joi')
-let mysql = require('mysql');
+
 const express = require('express');
 const app = express();
 
-let connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'sampleDB',});
 
-connection.connect(function(err) {
-  if (!!err) {
-    return console.error('error: ' + err.message);
-  }
-  else{
-
-  console.log('Connected to the MySQL server.');
-
-  }
-});
-
-
-app.get('/',function(req,res){
-
-  connection.query('SELECT * FROM sampleDB',function(  qerror,rows,fields  ){
-      if(error){
-        console.log('error');
-
-      }
-      else{
-        console.log('success');
-
-      }
-
-
-  });
+const Joi = require('joi')
+app.get('/',(req,res)=>{
+    res.send("hel  o")
 })
+
+
+
+// let mysql = require('mysql');
+// let connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'sampleDB',});
+
+// connection.connect(function(err) {
+//   if (!!err) {
+//     return console.error('error: ' + err.message);
+//   }
+//   else{
+
+//   console.log('Connected to the MySQL server.');
+
+//   }
+// });
+
+
+// app.get('/',function(req,res){
+
+//   connection.query('SELECT * FROM sampleDB',function(  qerror,rows,fields  ){
+//       if(error){
+//         console.log('error');
+
+//       }
+//       else{
+//         console.log('success');
+
+//       }
+
+
+//   });
+// })
 app.listen(1337);
